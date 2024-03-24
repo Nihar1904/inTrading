fbd38b1f546eaa168feb96f86c94bac10701ccb5
Nihar Namdev Sumbhe <sumbhenihar@gmail.com>
2024-03-24 17:32:53 +0530

    First Commit

--- inTrading/src/main/java/com/inTrading/model/Users.java
+++ inTrading/src/main/java/com/inTrading/model/Users.java
@@ -1,6 +1,5 @@
 package com.inTrading.model;
-//        CreateDatetime      DateTime
-//        UpdateDatetime      DateTime
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
